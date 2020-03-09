db8ebfa3d2549e4f804437db5c4fe76918895cfa
Tom Spencer <tom@homeflow.co.uk>
2020-03-09 10:54:49 +0000

    Add new private class LoyaltyPlan for extracting calculatePoints from generateStatement().

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,6 +1,14 @@
 import java.util.Vector;
 
 public class Customer {
+
+  private final Tariff tariff = new Tariff();
+  private final LoyaltyPlan loyaltyPlan = new LoyaltyPlan();
+  private String name;
+  private Vector<Rental> rentals = new Vector<>();
+
+
+
   public Customer(String name) {
     this.name = name;
   }
@@ -20,9 +28,9 @@ public class Customer {
     StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
 
     for (Rental rental : this.rentals) {
-      double rentalPrice = calculatePrice(rental);
+      double rentalPrice = tariff.calculatePrice(rental);
 
-      frequentRenterPoints += calculatePoints(rental);
+      frequentRenterPoints += loyaltyPlan.calculatePoints(rental);
 
       result.append("\t").append(rental.getMovie().getTitle()).append("\t").append(rentalPrice).append("\n");
       totalAmount += rentalPrice;
@@ -36,39 +44,42 @@ public class Customer {
     return result.toString();
   }
 
-  private int calculatePoints(Rental rental) {
-    int frequentRenterPoints = 0;
-    frequentRenterPoints++;
 
-    if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-            && rental.getDaysRented() > 1)
-      frequentRenterPoints++;
-    return frequentRenterPoints;
-  }
+  private class Tariff {
 
-  private double calculatePrice(Rental rental) {
-    double rentalPrice = 0;
+    private double calculatePrice(Rental rental) {
+      double rentalPrice = 0;
 
-    // determines the amount for each line
-    switch (rental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-        rentalPrice += 2;
-        if (rental.getDaysRented() > 2)
-          rentalPrice += (rental.getDaysRented() - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        rentalPrice += rental.getDaysRented() * 3;
-        break;
-      case Movie.CHILDRENS:
-        rentalPrice += 1.5;
-        if (rental.getDaysRented() > 3)
-          rentalPrice += (rental.getDaysRented() - 3) * 1.5;
-        break;
+      // determines the amount for each line
+      switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+          rentalPrice += 2;
+          if (rental.getDaysRented() > 2)
+            rentalPrice += (rental.getDaysRented() - 2) * 1.5;
+          break;
+        case Movie.NEW_RELEASE:
+          rentalPrice += rental.getDaysRented() * 3;
+          break;
+        case Movie.CHILDRENS:
+          rentalPrice += 1.5;
+          if (rental.getDaysRented() > 3)
+            rentalPrice += (rental.getDaysRented() - 3) * 1.5;
+          break;
+      }
+      return rentalPrice;
     }
-    return rentalPrice;
   }
 
+  private class LoyaltyPlan {
 
-  private String name;
-  private Vector<Rental> rentals = new Vector<>();
+    private int calculatePoints(Rental rental) {
+      int frequentRenterPoints = 0;
+      frequentRenterPoints++;
+
+      if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+              && rental.getDaysRented() > 1)
+        frequentRenterPoints++;
+      return frequentRenterPoints;
+    }
+  }
 }
